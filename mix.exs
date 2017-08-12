799c45e4291c349916e7ea2ee621c1bf84013c7f
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2017-08-12 04:14:00 -0400

    adding hex config

--- mix.exs
+++ mix.exs
@@ -1,13 +1,36 @@
 defmodule Bittrex.Mixfile do
   use Mix.Project
 
+  @elixir_version "~> 1.4"
+  @name :bittrex
+  @version "0.1.0"
+  @description """
+    Client for Bittrex (https://bittrex.com)
+  """
+  @source_url "https://github.com/straw-hat-llc/elixir_bittrex"
+
   def project do
-    [app: :bittrex,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    production? = Mix.env == :prod
+
+    [
+      app: @name,
+      description: @description,
+      version: @version,
+      elixir: @elixir_version,
+      build_embedded: production?,
+      start_permanent: production?,
+      deps: deps(),
+      package: package(),
+
+      # ex_docs
+      name: "Bittrex",
+      source_url: @source_url,
+      homepage_url: @source_url,
+      docs: [
+        main: "Bittrex",
+        extras: ["README.md"]
+      ]
+    ]
   end
 
   def application do
@@ -15,7 +38,26 @@ defmodule Bittrex.Mixfile do
   end
 
   defp deps do
-    [{:poison, "~> 3.1"},
-     {:httpoison, "~> 0.12"}]
+    [
+      {:poison, "~> 3.1"},
+      {:httpoison, "~> 0.12"},
+
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      name: @name,
+      files: [
+        "lib",
+        "mix.exs",
+        "README*",
+        "LICENSE*"
+      ],
+      maintainers: ["Yordis Prieto"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    ]
   end
 end
