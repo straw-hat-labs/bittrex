dc4951b41386928e1cad711d6ec18e7f31e4226e
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2017-08-13 21:26:18 -0400

    ADD traded at field to order

--- lib/bittrex/order.ex
+++ lib/bittrex/order.ex
@@ -5,7 +5,7 @@ defmodule Bittrex.Order do
   defstruct [:id, :quantity, :quantity_remaining, :price, :type,
              :total, :fill_type, :limit, :commision, :price_per_unit,
              :opened, :closed, :cancelation_initiated, :immediate_or_cancel,
-             :is_conditional, :market]
+             :is_conditional, :market, :traded_at]
 
   def cancel_order(order_id) do
     CancelOrder.call(order_id)
@@ -21,6 +21,7 @@ defmodule Bittrex.Order do
       id: get_order_id(item),
       price: get_price(item),
       type: type,
+      traded_at: Bittrex.format_datetime(item["TimeStamp"]),
       quantity: item["Quantity"],
       quantity_remaining: item["QuantityRemaining"],
       total: item["Total"],
