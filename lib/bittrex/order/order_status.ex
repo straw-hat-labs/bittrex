9fccb3a5e71230eb79fbbe8f1f64de19db4333e7
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2017-08-28 03:21:50 -0400

    fix credo

--- lib/bittrex/order/order_status.ex
+++ lib/bittrex/order/order_status.ex
@@ -4,13 +4,13 @@ defmodule Bittrex.Order.OrderStatus do
 
   defstruct [:name, :at]
 
-  def new(%{"Closed" => closed_at} = order) do
+  def new(%{"Closed" => closed_at} = _order) do
     %OrderStatus{
       name: "CLOSED",
       at: Bittrex.format_datetime(closed_at)
     }
   end
-  def new(%{"Opened" => opened_at} = order) do
+  def new(%{"Opened" => opened_at} = _order) do
     %OrderStatus{
       name: "OPENED",
       at: Bittrex.format_datetime(opened_at)
