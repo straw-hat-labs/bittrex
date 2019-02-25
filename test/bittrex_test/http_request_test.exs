2efcb4c7df22c3f4b4890866aebde47c94f5208f
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-02-24 19:02:13 -0800

    Fix test

--- test/bittrex_test/http_request_test.exs
+++ test/bittrex_test/http_request_test.exs
@@ -10,7 +10,8 @@ defmodule BittrexTest.HttpRequestTest do
 
   test "full_url/1" do
     full_url =
-      HttpRequest.new(:get, "/test", apikey: "1235")
+      :get
+      |> HttpRequest.new("/test", apikey: "1235")
       |> HttpRequest.full_url()
 
     assert full_url == "https://bittrex.com/api/v1.1/test?apikey=1235"
@@ -18,7 +19,8 @@ defmodule BittrexTest.HttpRequestTest do
 
   test "put_header/1" do
     request =
-      HttpRequest.new(:get, "/test", [], authorization: "bearer 123")
+      :get
+      |> HttpRequest.new("/test", [], authorization: "bearer 123")
       |> HttpRequest.put_header(apikey: "123")
 
     assert request.headers == [authorization: "bearer 123", apikey: "123"]
@@ -26,7 +28,8 @@ defmodule BittrexTest.HttpRequestTest do
 
   test "put_params/1" do
     request =
-      HttpRequest.new(:get, "/test", [nonce: "123"], [])
+      :get
+      |> HttpRequest.new("/test", [nonce: "123"], [])
       |> HttpRequest.put_params(apikey: "123")
 
     assert request.params == [nonce: "123", apikey: "123"]
