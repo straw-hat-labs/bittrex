6581f084ec80d1dd98c1c40942f7fc42db544b7c
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-03-08 00:24:05 -0800

    Fix http client

--- lib/bittrex/http_client.ex
+++ lib/bittrex/http_client.ex
@@ -2,7 +2,7 @@ defmodule Bittrex.HttpClient do
   alias Bittrex.HttpRequest
 
   @enforce_keys [:api_key, :api_secret]
-  defstruct [:api_key, :api_secret]
+  defstruct [:api_key, :api_secret, :sub_account_id]
 
   def new(api_key, api_secret) do
     %__MODULE__{
@@ -11,41 +11,30 @@ defmodule Bittrex.HttpClient do
     }
   end
 
+  def with_sub_account(http_client, sub_account_id) do
+    Map.put(http_client, :sub_account_id, sub_account_id)
+  end
+
+  @doc false
   def send(%HttpRequest{} = request) do
     request
-    |> put_default_params()
+    |> HttpRequest.put_required_headers()
     |> execute_request()
     |> process_response()
   end
 
-  defp put_default_params(request) do
-    timestamp = DateTime.utc_now() |> DateTime.to_unix(:millisecond)
-    content_hash = :crypto.hash(:sha256, "")
-    params = %{
-      "Api-Key" => request.http_client.api_key,
-      "Api-Timestamp" => timestamp,
-      "Api-Content-Hash" => content_hash,
-      "Api-Signature" => api_signature(request, timestamp, content_hash)
-    }
-
-    HttpRequest.put_params(request, params)
-  end
-
-  defp api_signature(request, timestamp, content_hash) do
-    IO.inspect(request)
-    pre_sign = "#{timestamp}#{request.url}#{content_hash}" |> IO.inspect()
-    :crypto.hmac(:sha512, request.http_client.api_key, pre_sign)
-  end
-
   defp execute_request(request) do
-    HTTPoison.request(request.method, request.url, "", request.headers, params: request.params)
+    headers = Enum.into(request.headers, [])
+    HTTPoison.request(request.method, request.url, request.body, headers, params: request.params)
   end
 
   defp process_response({:ok, %HTTPoison.Response{status_code: 200, body: body} = _response}) do
     decode_response(body)
   end
 
-  defp process_response({:ok, %HTTPoison.Response{status_code: status_code, body: body} = _response}) do
+  defp process_response(
+         {:ok, %HTTPoison.Response{status_code: status_code, body: body} = _response}
+       ) do
     {:error, status_code, body}
   end
 
@@ -56,7 +45,7 @@ defmodule Bittrex.HttpClient do
   defp decode_response(body) do
     case Jason.decode(body) do
       {:ok, data} -> {:ok, data}
-      _ -> {:error, "can't decode the response body"}
+      _ -> {:error, :decoding_body_failed}
     end
   end
 end
