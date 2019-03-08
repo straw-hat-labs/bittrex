6c803082ba5ce37ba3a0910213fade19f331043e
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-03-08 00:22:57 -0800

    Fix http request

--- lib/bittrex/http_request.ex
+++ lib/bittrex/http_request.ex
@@ -1,30 +1,100 @@
 defmodule Bittrex.HttpRequest do
   @base_url Application.get_env(:bittrex, :base_url, "https://api.bittrex.com/v3")
 
-  @enforce_keys [:http_client, :method, :url, :headers, :params]
-  defstruct [:http_client, :method, :url, :headers, :params]
+  defstruct [:http_client, :method, :url, :headers, :params, :body]
 
-  def new(http_client, method, path, params \\ %{}, headers \\ %{}) do
+  @doc false
+  def new(http_client) do
     %__MODULE__{
       http_client: http_client,
-      method: method,
-      url: process_url(path),
-      params: params,
-      headers: headers
+      headers: %{},
+      params: %{},
+      body: ""
     }
   end
 
+  @doc false
+  def put_method(%__MODULE__{} = http_request, method) do
+    Map.put(http_request, :method, method)
+  end
+
+  @doc false
+  def put_path(%__MODULE__{} = http_request, path) do
+    url = process_url(path)
+    Map.put(http_request, :url, url)
+  end
+
+  @doc false
+  def put_body(%__MODULE__{} = http_request, body) do
+    Map.put(http_request, :body, Jason.encode!(body))
+  end
+
+  @doc false
   def process_url(path) do
     @base_url <> path
   end
 
-  def put_header(%__MODULE__{} = request, headers) do
+  @doc false
+  def put_headers(%__MODULE__{} = request, headers) do
     headers = Map.merge(request.headers, headers)
     Map.put(request, :headers, headers)
   end
 
+  @doc false
   def put_params(%__MODULE__{} = request, params) do
     params = Map.merge(request.params, params)
     Map.put(request, :params, params)
+  end
+
+  @doc false
+  def put_required_headers(request) do
+    headers =
+      %{}
+      |> put_api_key_header(request)
+      |> put_api_timestamp_header(request)
+      |> put_api_content_hash_header(request)
+      |> put_api_signature_header(request)
+      |> put_api_sub_account_id(request)
+
+    put_headers(request, headers)
+  end
+
+  defp put_api_sub_account_id(headers, request) do
+    if request.http_client.sub_account_id == nil do
+      headers
+    else
+      Map.put(headers, "Api-Subaccount-Id", request.http_client.sub_account_id)
+    end
+  end
+
+  defp put_api_signature_header(headers, request) do
+    signature = api_signature(request, headers["Api-Timestamp"], headers["Api-Content-Hash"])
+    Map.put(headers, "Api-Signature", signature)
+  end
+
+  defp put_api_key_header(headers, request) do
+    Map.put(headers, "Api-Key", request.http_client.api_key)
+  end
+
+  defp put_api_timestamp_header(headers, request) do
+    timestamp = :os.system_time(:milli_seconds)
+    Map.put(headers, "Api-Timestamp", timestamp)
+  end
+
+  defp put_api_content_hash_header(headers, request) do
+    content_hash =
+      :sha512
+      |> :crypto.hash(request.body)
+      |> Base.encode16(case: :lower)
+
+    Map.put(headers, "Api-Content-Hash", content_hash)
+  end
+
+  defp api_signature(request, timestamp, content_hash) do
+    pre_sign = "#{timestamp}#{request.url}#{content_hash}"
+
+    :sha512
+    |> :crypto.hmac(request.http_client.api_secret, pre_sign)
+    |> Base.encode16(case: :lower)
   end
 end
