39b0837c94c5ba5fc81a5968a592d4d39882088d
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-03-09 09:56:47 -0800

    Fix test case

--- test/bittrex_test/addresses_test.exs
+++ test/bittrex_test/addresses_test.exs
@@ -2,9 +2,9 @@ defmodule Bittrex.Test.AddressesTest do
   use Bittrex.TestSupport.CaseTemplate, async: true
   alias Bittrex.Addresses
 
-  test "GET /addresses" do
+  test "GET /addresses NOT IMPLEMENTED" do
     use_cassette "get_addresses" do
-      assert {:ok, account} = with_mock_client() |> Addresses.get_addresses()
+      assert {:error, _status_code, _body} = with_mock_client() |> Addresses.get_addresses()
     end
   end
 end
