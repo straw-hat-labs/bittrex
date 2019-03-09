42d42ea24dd4a5173b64c43162394494e4118d0b
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-03-09 09:28:48 -0800

    Remove dead code

--- lib/bittrex.ex
+++ lib/bittrex.ex
@@ -3,57 +3,6 @@ defmodule Bittrex do
   Contains a functions for credentials and commo utility functions.
   """
 
-  def api_signature(url, content_hash) do
-    # var uri = 'https://api.bittrex.com/v3/balances';
-    # var preSign = [timestamp, uri, contentHash].join('');
-    # var signature = CryptoJS.HmacSHA512(preSign, apiSecret).toString(CryptoJS.enc.ASCII);
-  end
-
-  @doc """
-  Get the credentials of Bittrex.
-  """
-  @spec credentials() :: keyword
-  def credentials do
-    Application.fetch_env!(:bittrex, :credentials)
-  end
-
-  @doc """
-  Get the API Key from the credentials.
-  """
-  @spec api_key() :: String.t()
-  def api_key do
-    credentials() |> Keyword.get(:api_key)
-  end
-
-  @doc """
-  Get the API Secret from the credentials
-  """
-  @spec api_secret() :: String.t()
-  def api_secret do
-    credentials() |> Keyword.get(:api_secret)
-  end
-
-  @doc """
-  Returns the encrypted API Sign header value.
-  """
-  @spec get_api_sign(String.t()) :: String.t()
-  def get_api_sign(url) do
-    secret = api_secret()
-
-    :sha512
-    |> :crypto.hmac(secret, url)
-    |> Base.encode16()
-    |> String.downcase()
-  end
-
-  @doc """
-  Nonce used for the authorization.
-  """
-  @spec nonce() :: integer
-  def nonce do
-    :os.system_time(:milli_seconds)
-  end
-
   @doc """
   Format Bittrex date time to Elixir `t:NaiveDateTime.t/0`.
   """
