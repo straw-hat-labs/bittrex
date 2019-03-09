289eb1b9bd7e8a77499d15e69be6449d2257caea
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-03-09 09:51:30 -0800

    Fix output folder

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,8 @@
 use Mix.Config
 
 config :exvcr, [
-  vcr_cassette_library_dir: "test/fixtures/vcr_cassettes",
-  custom_cassette_library_dir: "test/fixtures/custom_vcr_cassettes",
+  vcr_cassette_library_dir: "test/support/fixtures/vcr_cassettes",
+  custom_cassette_library_dir: "test/support/fixtures/custom_vcr_cassettes",
   filter_sensitive_data: [],
   filter_url_params: false,
   filter_request_headers: [],
