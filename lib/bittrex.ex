2003cb035edf1f2e713ea00365ea2ff612c27eed
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-03-14 23:45:30 -0700

    Fix docs

--- lib/bittrex.ex
+++ lib/bittrex.ex
@@ -1,14 +1,11 @@
 defmodule Bittrex do
   @moduledoc """
-  Contains a functions for credentials and commo utility functions.
+  Contains a functions for credentials and common utility functions.
   """
 
-  @doc """
-  Format Bittrex date time to Elixir `t:NaiveDateTime.t/0`.
-  """
-  @spec format_datetime(nil) :: nil
+  @doc false
   def format_datetime(nil), do: nil
-  @spec format_datetime(String.t()) :: NaiveDateTime.t() | nil
+
   def format_datetime(datetime_string) do
     case NaiveDateTime.from_iso8601(datetime_string) do
       {:ok, date} -> date
@@ -16,6 +13,7 @@ defmodule Bittrex do
     end
   end
 
+  @doc false
   def camelcase_keys(struct) do
     StrawHat.Utils.Map.deep_map(struct, &camelize_key/1, &StrawHat.identity/1)
   end
