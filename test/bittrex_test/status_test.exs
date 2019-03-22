e9d87af7cb3d7b5009364be29f05d9157a8d9c51
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-03-22 01:56:44 -0700

    Fix status tests

--- test/bittrex_test/status_test.exs
+++ test/bittrex_test/status_test.exs
@@ -3,8 +3,14 @@ defmodule Bittrex.StatusTest do
   alias Bittrex.{Status, ServiceStatus}
 
   test "GET /status" do
-    use_cassette "get_status" do
-      assert {:ok, %ServiceStatus{}} = with_mock_client() |> Status.get_status()
-    end
+    stub_request(%{
+      "status" => "123",
+      "serverTime" => 12351243
+    })
+
+    assert {:ok, %ServiceStatus{
+      status: "123",
+      server_time: 12351243
+    }} = with_mock_client() |> Status.get_status()
   end
 end
